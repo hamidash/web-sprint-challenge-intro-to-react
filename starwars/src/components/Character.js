d5e8274c2f80f245708698e2aedf6aa6093b3986
Hamida Sharifova <hamida@Hamidas-MacBook-Pro.local>
2020-07-31 20:24:47 -0700

    Intro-React Sprint Challenge

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,61 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const CharacterDiv = styled.div`
+    border: 2px solid black;
+    width: 40%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 1% 30%;
+    border-radius: 10% 30% 30%;
+    padding: 2%;
+    box-shadow: 2px 2px gray;
+    background-color: rgba(200,100,0,0.2);
+    color: blue;
+
+    @media(max-width: 500px) {
+        background-image: url('https://www.news.gatech.edu/sites/default/files/pictures/feature_images/star_wars_002.jpg');
+    }
+`;
+
+const NameH3 = styled.h3`
+    font-size: 2.5 rem;
+    
+`;
+
+const BirthP = styled.p`
+    font-size: 1.5 rem;
+`;
+
+const GenderP = styled.p`
+    font-size: 1.5 rem;
+`;
+
+
+
+
+
+function Character(props) {
+ const {movieData} = props;
+
+ //console.log(movieData[0]);
+
+ return(
+
+    movieData.map((item, index) => { 
+        return <CharacterDiv key={"01"}>
+                    <NameH3>Name: {item.name}</NameH3>
+                    <BirthP>Birth:{item.birth_year}</BirthP>
+                    <GenderP>Gender: {item.gender} </GenderP>
+                </CharacterDiv>
+    })
+
+    
+ )
+}
+
+
+export default Character;
